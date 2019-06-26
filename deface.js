500cb55d93037f86717d48210cc74f3379e3155d
lb74 <huuhan96@gmail.com>
2019-06-26 13:59:21 +0700

    Create deface.js

---
+++ deface.js
@@ -0,0 +1,4 @@
+document.body.innerHTML ='<div style="height:100px;"></div><div style="text-align:center;font-size: 20px;color: #b11e1e;"><img src="http://cnsc.uit.edu.vn/img/cnsc_logo_v2.png" style="height:150px;">' + 
+'<div>Website của bạn đã bị thay đổi giao diện với mục đánh giá an toàn thông tin cho Website</div>' +
+'<div>Việc thực hiện đánh giá an toàn thông tin được sự cấp phép của đơn vị quản lý Website</div>' +
+'</div>';
